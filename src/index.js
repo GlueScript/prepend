ce4adfca1d53bef3c17d93b1d7dde144328a39b5
Tim Rodger <tim.rodger@gmail.com>
2014-12-02 00:22:56 +0000

    Commits raw code

---
+++ src/index.js
@@ -0,0 +1,31 @@
+var app = require('express')(),
+    bodyParser = require('body-parser'),
+    filter = require('./lib/filter');
+
+app.use(bodyParser.text({type : 'text/*', limit: '1024kb'}));
+app.use(bodyParser.text({type : 'application/xml'}));
+
+// use env.PORT if set
+var PORT = 8782;
+
+// App
+app.get('/', function (req, res) {
+    res.json({"description":"Prepend - prepends the posted json array with the string supplied via prepend query param"});
+});
+
+app.post('/', function(req, res) {
+    // validation? let the filter module do that?
+    
+    // get document from request body
+    // get filter from query string
+    // invoke business logic using doc and filter
+    var result = filter.prepend(req.body, req.param('prepend'));
+
+    // respond with json array of 0+ items
+    res.setHeader('Content-Type', 'application/json');
+    res.send(result);
+});
+
+app.listen(PORT);
+
+console.log('Running on http://localhost:' + PORT);
---
+++ src/lib/filter.js
@@ -0,0 +1,18 @@
+var xpath = require('xpath'),
+    parser = require('xmldom').DOMParser;
+
+/*
+ */
+exports.prepend = function(input, prepend) {
+
+    var items = [];
+
+    try {
+        // iterate over input items
+
+    } catch (e){
+        console.log(e);
+    }
+
+    return items;
+};
