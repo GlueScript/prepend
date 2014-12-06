32197c1d7ce9268ab61db74c88066d33b8545133
Tim Rodger <tim.rodger@gmail.com>
2014-12-06 09:52:49 +0000

    Adds winston logging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 var app = require('express')(),
+    winston = require('winston'),
     bodyParser = require('body-parser'),
     filter = require('./lib/filter');
+
+/*
+* Get winston to log uncaught exceptions and to not exit
+*/
+var logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      handleExceptions: true
+    })
+  ],
+  exitOnError: false
+});
 
 app.use(bodyParser.json({limit: '1024kb'}));
 
@@ -23,4 +36,4 @@ var PORT = process.env.PORT || 80;
 
 app.listen(PORT);
 
-console.log('Running on http://localhost:' + PORT);
+logger.log('Running on http://localhost:' + PORT);
